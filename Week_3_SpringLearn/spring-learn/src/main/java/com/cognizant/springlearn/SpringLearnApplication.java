7bf1535e27c64a7904768b8f582b6113528a9d67
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-07-12 12:55:49 +0530

    Completed Week 3 - Hands-on 5: Hello World RESTful Web Service

--- Week_3_SpringLearn/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week_3_SpringLearn/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,19 +1,19 @@
 package com.cognizant.springlearn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@SpringBootApplication
 public class SpringLearnApplication {
-
-	private static final Logger LOGGER =
-			LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
 
-		displayCountry();
+		// Uncomment this only for Hands-on 4
+		// displayCountry();
 
+		SpringApplication.run(SpringLearnApplication.class, args);
 	}
 
 	public static void displayCountry() {
