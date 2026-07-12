9332f875f1328ae42b3cc6cfef46e49d4a5bb72a
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-07-12 12:37:00 +0530

    Hand's on 4 is completed for Week 3

--- Week_3_SpringLearn/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week_3_SpringLearn/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,37 @@
-package com.cognizant.spring_learn;
+package com.cognizant.springlearn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class SpringLearnApplication {
 
+	private static final Logger LOGGER =
+			LoggerFactory.getLogger(SpringLearnApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
+
+		displayCountry();
+
 	}
 
+	public static void displayCountry() {
+
+		try {
+			System.out.println("Loading XML...");
+
+			ApplicationContext context =
+					new ClassPathXmlApplicationContext("country.xml");
+
+			System.out.println("XML Loaded");
+
+			Country country = context.getBean("country", Country.class);
+
+			System.out.println(country);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
