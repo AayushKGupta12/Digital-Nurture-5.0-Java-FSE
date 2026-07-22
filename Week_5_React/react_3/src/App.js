8046c56cea0fa6213a4313286bdfee143ac3b81f
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-07-23 00:15:35 +0530

    React 2 & 3 Completed

---
+++ Week_5_React/react_3/src/App.js
@@ -0,0 +1,22 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <CalculateScore
+                Name={"Aayush Kumar Gupta"}
+                School={"St Karens High School"}
+                total={284}
+                goal={3}
+            />
+
+        </div>
+
+    );
+
+}
+
+export default App;
